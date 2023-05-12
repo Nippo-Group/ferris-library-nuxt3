cb55ef688448e0309cdde96e73c47971bbf836be
shu-saginoya <saginoya@create-nippo.co.jp>
2023-05-12 13:44:47 +0900

    Update - Error page Modification

---
+++ error.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const error = useError();
+</script>
+
+<template>
+  <v-container>
+    <text-page-title>
+      {{ error?.message }}
+    </text-page-title>
+
+    <btn-inside link="ホームへもどる" to="/" class="mb-2"></btn-inside>
+    <btn-inside
+      link="Return to home page"
+      to="/english"
+      class="mb-2"
+    ></btn-inside>
+  </v-container>
+</template>
