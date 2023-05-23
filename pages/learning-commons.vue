8d8cb3a5bd15b65b7d8f7a2f1b0dc8b6c536b30f
shu-saginoya <saginoya@create-nippo.co.jp>
2023-05-23 16:29:56 +0900

    Update - Learning commons

--- pages/learning-commons.vue
+++ pages/learning-commons.vue
@@ -1,3 +1,23 @@
+<script setup>
+const title = ref("ラーニングコモンズ");
+useSeoMeta({ title: title.value });
+
+const items = [
+  {
+    title: "ラーニングコモンズの紹介",
+    href: "https://www.ferris.ac.jp/information/library/learningcommons.html",
+    img: "learning-commons-01.jpg",
+  },
+  {
+    title: "ピアノの弾ける図書館",
+    href: "https://magazine.ferris.ac.jp/20210202/7203/",
+    img: "learning-commons-02.jpg",
+  },
+];
+
+const directory = "learning-commons";
+</script>
+
 <template>
   <v-container>
     <text-page-title>{{ title }}</text-page-title>
@@ -5,44 +25,19 @@
       <v-col v-for="(item, i) in items" :key="i" cols="12" md="6">
         <v-card :href="item.href" target="_blank" dark>
           <v-img
-            :src="item.img"
+            :src="generateImgPath(item.img, directory)"
             class="align-end"
             gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
             height="200px"
+            cover
           >
-            <v-card-title
-              >{{ item.title
-              }}<v-icon class="ml-2">mdi-open-in-new</v-icon></v-card-title
-            >
+            <v-card-title class="text-white">
+              {{ item.title }}
+              <icons-open-in-new></icons-open-in-new>
+            </v-card-title>
           </v-img>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
-
-<script>
-export default {
-  name: "PageLearningCommons",
-  data: () => ({
-    title: "ラーニングコモンズ",
-    items: [
-      {
-        title: "ラーニングコモンズの紹介",
-        href: "https://www.ferris.ac.jp/information/library/learningcommons.html",
-        img: "~/assets/images/learning-commons/learning-commons-01.jpg",
-      },
-      {
-        title: "ピアノの弾ける図書館",
-        href: "https://www.ferris.ac.jp/blog/20210202/7203/",
-        img: "~/assets/images/learning-commons/learning-commons-02.jpg",
-      },
-    ],
-  }),
-  head() {
-    return {
-      title: this.title,
-    };
-  },
-};
-</script>
