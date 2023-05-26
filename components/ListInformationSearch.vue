84ff7e3df9a879623b3bce3cf81b6af62873bb64
shu-saginoya <saginoya@create-nippo.co.jp>
2023-05-26 17:05:09 +0900

    Update - Information search page

--- components/ListInformationSearch.vue
+++ components/ListInformationSearch.vue
@@ -1,53 +1,53 @@
 <script setup>
-import { useLanguageStore } from "@/stores/language";
-
+import { mdiOpenInNew, mdiBookSearchOutline } from "@mdi/js";
 const props = defineProps({
   items: {
     type: Array,
     default: () => {},
   },
 });
-const active = ref(1);
-const langStore = useLanguageStore();
-const language = ref(langStore.language);
 </script>
 
 <template>
-  <v-stepper v-model="active" vertical non-linear>
-    <template v-for="(item, i) in props.items" :key="'item-' + i">
-      <v-stepper-step editable :complete="active > item.step" :step="item.step">
-        {{ item.subTitle }}
-      </v-stepper-step>
-      <v-stepper-content :step="item.step">
-        <ul class="link-list">
-          <li v-for="(listItem, j) in item.list" :key="'listItem' + i + j">
-            <v-btn
-              v-if="listItem.name"
-              class="text-none"
-              :to="listItem.push"
-              :href="listItem.href"
-              :target="listItem.href ? '_blank' : '_self'"
-              outlined
-            >
-              {{ listItem.name }}
-              <v-icon v-show="listItem.icon" right> {{ listItem.icon }}</v-icon>
-            </v-btn>
-            <p class="grey--text">{{ listItem.comment }}</p>
-          </li>
-        </ul>
-        <div class="text-right">
-          <v-btn color="primary" @click="active = item.step + 1">
-            {{ language === "en" ? "Next" : "つぎへ" }}
-          </v-btn>
-        </div>
-      </v-stepper-content>
-    </template>
-  </v-stepper>
+  <v-container>
+    <v-row>
+      <v-col v-for="(item, i) in props.items" :key="'item-' + i" cols="12">
+        <v-card :title="item.subTitle" :prepend-icon="mdiBookSearchOutline">
+          <v-card-text>
+            <ul class="link-list">
+              <li v-for="(listItem, j) in item.list" :key="'listItem' + i + j">
+                <v-btn
+                  v-if="listItem.name"
+                  class="text-none"
+                  color="primary"
+                  :to="listItem.push"
+                  :href="listItem.href"
+                  :target="listItem.href ? '_blank' : '_self'"
+                  variant="tonal"
+                >
+                  {{ listItem.name }}
+                  <v-icon
+                    v-show="listItem.openInNew"
+                    end
+                    :icon="mdiOpenInNew"
+                  ></v-icon>
+                </v-btn>
+                <p class="text-grey">{{ listItem.comment }}</p>
+              </li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
 .link-list {
   list-style: none;
   padding-left: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 }
 </style>
