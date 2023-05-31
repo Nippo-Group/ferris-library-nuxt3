fd9c104b019b4751d7b6140088f36a7462dcde9e
shu-saginoya <saginoya@create-nippo.co.jp>
2023-05-31 15:40:05 +0900

    Update - Links collection page

--- components/ListLinksCollection.vue
+++ components/ListLinksCollection.vue
@@ -1,19 +1,32 @@
+<script setup>
+const props = defineProps({
+  items: {
+    type: Array,
+    default: () => {},
+  },
+  category: {
+    type: String,
+    default: null,
+  },
+});
+</script>
+
 <template>
   <v-row>
     <v-col
-      v-for="item in items"
-      v-show="item.category === category"
+      v-for="item in props.items"
+      v-show="item.category === props.category"
       :key="item.id"
       cols="12"
       lg="6"
     >
       <v-card :href="item.url" target="_blank">
         <v-card-title>
-          <v-chip small class="mr-2">
-            {{ item.overseas ? '海外' : '国内' }}
+          <v-chip class="mr-2" color="primary" variant="tonal">
+            {{ item.overseas ? "海外" : "国内" }}
           </v-chip>
           {{ item.name }}
-          <v-icon right> mdi-open-in-new </v-icon>
+          <icons-open-in-new end size="x-small" />
         </v-card-title>
         <v-card-subtitle v-show="item.author">
           {{ item.author }}
@@ -25,20 +38,3 @@
     </v-col>
   </v-row>
 </template>
-
-<script>
-export default {
-  name: 'ListLinksCollection',
-  props: {
-    items: {
-      type: Array,
-      default: () => {},
-    },
-    category: {
-      type: String,
-      default: null,
-    },
-  },
-  data: () => ({}),
-}
-</script>
