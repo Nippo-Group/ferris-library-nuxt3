95e7cc5895ab3add06a24e317bd68eb0eb93aa55
shu-saginoya <saginoya@create-nippo.co.jp>
2023-06-15 15:42:42 +0900

    App - icons component

---
+++ components/icons/CalendarToday.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { mdiCalendarToday } from "@mdi/js";
+</script>
+
+<template>
+  <v-icon :icon="mdiCalendarToday"></v-icon>
+</template>
---
+++ components/icons/ChevronLeft.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { mdiChevronLeft } from "@mdi/js";
+</script>
+
+<template>
+  <v-icon :icon="mdiChevronLeft"></v-icon>
+</template>
