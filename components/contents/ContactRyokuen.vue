bb6ed95e203f0a88135b3b745865f7e444c2765d
saginoya <saginoya@create-nippo.co.jp>
2023-11-06 16:53:12 +0900

    Update - Contact page

---
+++ components/contents/ContactRyokuen.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import imgRyokuen from "~/assets/images/contact/contact-ryokuen.jpg";
+
+const name = "緑園本館";
+const addressNumber = "245-8651";
+const address = "横浜市泉区緑園4-5-3";
+const googleMap = "https://goo.gl/maps/EN6qf9SdPTx8woMv6";
+const tel = "045 (812) 6999";
+const fax = "045 (812) 9772";
+const eMali = "r_library@ferris.ac.jp";
+const image = imgRyokuen;
+const accesses = [
+  {
+    transportation: "相鉄いずみ野線",
+    root: "「緑園都市」下車徒歩3分",
+    icon: "train",
+  },
+  {
+    transportation: "神奈中バス",
+    root: "「フェリス女学院」下車徒歩1分",
+    icon: "car",
+  },
+];
+</script>
+
+<template>
+  <templates-card-contact
+    :name="name"
+    :accesses="accesses"
+    :address="address"
+    :address-number="addressNumber"
+    :e-mali="eMali"
+    :fax="fax"
+    :google-map="googleMap"
+    :image="image"
+    :tel="tel"
+  ></templates-card-contact>
+</template>
---
+++ components/contents/ContactYamate.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import imgYamate from "~/assets/images/contact/contact-yamate.jpg";
+
+const name = "山手分室";
+const addressNumber = "231-8651";
+const address = "横浜市中区山手町37";
+const googleMap = "https://goo.gl/maps/8KJm93i6qN7kL6Eb8";
+const tel = "045 (681) 5149";
+const fax = "045 (681) 5188";
+const eMali = "y_library@ferris.ac.jp";
+const image = imgYamate;
+const accesses = [
+  {
+    transportation: "JR京浜東北（根岸）線",
+    root: "「石川町」下車徒歩10分",
+    icon: "train",
+  },
+];
+</script>
+
+<template>
+  <templates-card-contact
+    :name="name"
+    :accesses="accesses"
+    :address="address"
+    :address-number="addressNumber"
+    :e-mali="eMali"
+    :fax="fax"
+    :google-map="googleMap"
+    :image="image"
+    :tel="tel"
+  ></templates-card-contact>
+</template>
