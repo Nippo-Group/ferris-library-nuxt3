bb8746591e7f1590564e646e4214d31b217c9751
saginoya <saginoya@create-nippo.co.jp>
2023-11-07 13:39:00 +0900

    Update - Pamphlet page

---
+++ components/contents/StudentPamphlet.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const tab = ref(null);
+</script>
+
+<template>
+  <v-tabs v-model="tab" color="primary" grow>
+    <v-tab>緑園本館</v-tab>
+    <v-tab>山手分室</v-tab>
+  </v-tabs>
+
+  <v-window v-model="tab">
+    <v-window-item>
+      <the-pdf-viewer
+        src="/documents/pamphlet/pamphlet_ryokuen.pdf"
+      ></the-pdf-viewer>
+    </v-window-item>
+    <v-window-item>
+      <the-pdf-viewer
+        src="/documents/pamphlet/pamphlet_yamate.pdf"
+      ></the-pdf-viewer>
+    </v-window-item>
+  </v-window>
+</template>
--- pages/pamphlet.vue
+++ pages/pamphlet.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 const title = ref("学生向けパンフレット");
 useSeoMeta({ title: title.value });
-
-const tab = ref(null);
 </script>
 
 <template>
@@ -11,23 +9,7 @@ const tab = ref(null);
     <p>在学生対象のパンフレットです</p>
     <v-row>
       <v-col cols="12" lg="10" xl="6">
-        <v-tabs v-model="tab" color="primary" grow>
-          <v-tab>緑園本館</v-tab>
-          <v-tab>山手分室</v-tab>
-        </v-tabs>
-
-        <v-window v-model="tab">
-          <v-window-item>
-            <the-pdf-viewer
-              src="/documents/pamphlet/pamphlet_ryokuen.pdf"
-            ></the-pdf-viewer>
-          </v-window-item>
-          <v-window-item>
-            <the-pdf-viewer
-              src="/documents/pamphlet/pamphlet_yamate.pdf"
-            ></the-pdf-viewer>
-          </v-window-item>
-        </v-window>
+        <contents-student-pamphlet />
       </v-col>
     </v-row>
   </v-container>
