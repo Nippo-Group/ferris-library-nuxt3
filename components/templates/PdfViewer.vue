ca9375be12a7e8835fa0751c42335698ee38a6e4
saginoya <saginoya@create-nippo.co.jp>
2023-11-29 16:33:15 +0900

    Update - PDFViewer component

--- components/templates/PdfViewer.vue
+++ components/templates/PdfViewer.vue
@@ -33,7 +33,9 @@ const fileName = computed(() => {
       </v-col>
       <v-col :cols="12">
         <v-sheet color="grey-lighten-5" class="pa-1">
-          <VuePDF :pdf="pdf" :page="page" fit-parent />
+          <client-only>
+            <VuePDF :pdf="pdf" :page="page" fit-parent />
+          </client-only>
         </v-sheet>
       </v-col>
       <v-col :cols="12" class="text-center">
