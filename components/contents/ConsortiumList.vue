b879c2057f8842d3a9cd6eb75babc6372593b29e
saginoya <saginoya@create-nippo.co.jp>
2023-12-05 16:36:09 +0900

    L23-17 Update consortium page

--- components/contents/ConsortiumList.vue
+++ components/contents/ConsortiumList.vue
@@ -2,66 +2,54 @@
 const items = [
   {
     name: "神奈川大学図書館",
-    url: "/documents/consortium/kanagawa.pdf",
-    type: "PDF",
+    url: "https://www.kanagawa-u.ac.jp/library/",
   },
   {
     name: "関東学院大学図書館",
-    url: "/documents/consortium/kangaku_hon.pdf",
-    type: "PDF",
+    url: "https://kgulibrary.kanto-gakuin.ac.jp/",
   },
   {
     name: "慶應義塾大学日吉メディアセンター",
-    url: "/documents/consortium/keio.pdf",
-    type: "PDF",
+    url: "https://www.lib.keio.ac.jp/hiyoshi",
   },
   {
     name: "國學院大學たまプラーザ図書館",
-    url: "/documents/consortium/kokugakuin.pdf",
-    type: "PDF",
+    url: "https://www.kokugakuin.ac.jp/student/lifesupport/library",
   },
   {
     name: "鶴見大学図書館",
-    url: "/documents/consortium/turumi.pdf",
-    type: "PDF",
+    url: "https://www.tsurumi-u.ac.jp/site/library-official/",
   },
   {
     name: "桐蔭横浜大学大学情報センター",
-    url: "/documents/consortium/toin.pdf",
-    type: "PDF",
+    url: "http://ufinity.toin.ac.jp/",
   },
   {
     name: "東京都市大学横浜キャンパス図書館",
-    url: "/documents/consortium/toshi.pdf",
-    type: "PDF",
+    url: "https://library.tcu.ac.jp/",
   },
   {
     name: "東洋英和女学院大学図書館",
-    url: "/documents/consortium/toyoeiwa.pdf",
-    type: "PDF",
+    url: "https://www.toyoeiwa.ac.jp/daigaku/shisetsu/library",
   },
   {
     name: "フェリス女学院大学附属図書館",
   },
   {
     name: "明治学院大学横浜校舎図書館",
-    url: "/documents/consortium/meigaku.pdf",
-    type: "PDF",
+    url: "https://www.meijigakuin.ac.jp/library/",
   },
   {
     name: "横浜国立大学附属図書館",
-    url: "/documents/consortium/yokokoku.pdf",
-    type: "PDF",
+    url: "http://www.lib.ynu.ac.jp/",
   },
   {
     name: "横浜商科大学図書館",
-    url: "/documents/consortium/yokosho.pdf",
-    type: "PDF",
+    url: "http://library.shodai.ac.jp/",
   },
   {
     name: "横浜市立大学学術情報センター",
-    url: "/documents/consortium/yokoichi.pdf",
-    type: "PDF",
+    url: "https://opac.yokohama-cu.ac.jp/drupal/",
   },
 ];
 </script>
@@ -69,6 +57,19 @@ const items = [
 <template>
   <v-card>
     <v-card-title> 大学名一覧（50音順） </v-card-title>
-    <templates-list-file :items="items"></templates-list-file>
+    <v-list>
+      <v-list-item
+        v-for="(item, index) in items"
+        :key="index"
+        :link="Boolean(item.url)"
+        :href="item.url"
+        target="_blank"
+        :title="item.name"
+      >
+        <template #append>
+          <icons-open-in-new v-if="item.url"></icons-open-in-new>
+        </template>
+      </v-list-item>
+    </v-list>
   </v-card>
 </template>
