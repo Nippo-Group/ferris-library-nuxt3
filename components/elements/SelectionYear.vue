7c687814f3258f8a9d6bf225aa3b19ecf9b82152
saginoya <saginoya@create-nippo.co.jp>
2023-12-19 17:02:15 +0900

    Update SelectionYear component

--- components/elements/SelectionYear.vue
+++ components/elements/SelectionYear.vue
@@ -17,14 +17,38 @@ const value = computed({
     emit("update:modelValue", value);
   },
 });
+
+const root = ref<HTMLDivElement>();
+const width = computed(() => root.value?.offsetWidth);
+const sp = computed(() => {
+  if (width.value) {
+    return width.value < 550;
+  } else {
+    return true;
+  }
+});
 </script>
 
 <template>
-  <div class="d-flex align-center flex-column bg-grey-lighten-4 pa-1">
-    <v-btn-toggle v-model="value" divided color="primary" density="compact">
-      <v-btn v-for="item in items" :key="item.value" :value="item.value">
-        {{ item.label }}
-      </v-btn>
-    </v-btn-toggle>
+  <div ref="root">
+    <div
+      v-if="!sp"
+      class="d-flex align-center flex-column bg-grey-lighten-4 pa-1"
+    >
+      <v-btn-toggle v-model="value" divided color="primary" density="compact">
+        <v-btn v-for="item in items" :key="item.value" :value="item.value">
+          {{ item.label }}
+        </v-btn>
+      </v-btn-toggle>
+    </div>
+    <v-select
+      v-else
+      v-model="value"
+      label="年度"
+      item-title="label"
+      item-value="value"
+      :items="items"
+      variant="outlined"
+    ></v-select>
   </div>
 </template>
