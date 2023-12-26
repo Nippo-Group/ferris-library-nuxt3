e299c66a043cc96196f2dadef34ced6d007b9432
saginoya <saginoya@create-nippo.co.jp>
2023-12-26 15:22:38 +0900

    Fixes LayoutWrapper

--- components/contents/LayoutWrapper.vue
+++ components/contents/LayoutWrapper.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { useLanguage } from "@/composable/language/useLanguage";
-import { useLoading } from "@/composable/layout/useLoading";
+
 import menusJapanese from "@/assets/json/menu.json";
 import menusEnglish from "@/assets/json/menu-english.json";
 
 const { langState } = useLanguage();
-const { loading } = useLoading();
 
 const copyright = "Ferris University Library";
 
@@ -21,36 +20,30 @@ const menus = computed(() => {
 
 <template>
   <v-app id="inspire">
-    <v-skeleton-loader
-      :loading="loading"
-      max-width="800"
-      type="list-item-avatar, ossein, paragraph"
-    >
-      <templates-navigation-drawer :menus="menus"></templates-navigation-drawer>
-      <templates-a-header>
-        <img
-          v-if="langState === 'en'"
-          src="~/assets/images/layouts/site-logo-english.png"
-          class="ferris-logo"
-          width="120"
-          height="34"
-          alt="Ferris University Library"
-        />
-        <img
-          v-if="langState === 'ja'"
-          src="~/assets/images/layouts/site-logo.png"
-          class="ferris-logo"
-          width="200"
-          height="65"
-          alt="フェリス女学院大学附属図書館"
-        />
-      </templates-a-header>
-      <v-main>
-        <slot />
-      </v-main>
-      <templates-a-footer :copyright="copyright"></templates-a-footer>
-      <templates-to-top></templates-to-top>
-      <templates-confirm-download></templates-confirm-download>
-    </v-skeleton-loader>
+    <templates-navigation-drawer :menus="menus"></templates-navigation-drawer>
+    <templates-a-header>
+      <img
+        v-if="langState === 'en'"
+        src="~/assets/images/layouts/site-logo-english.png"
+        class="ferris-logo"
+        width="120"
+        height="34"
+        alt="Ferris University Library"
+      />
+      <img
+        v-if="langState === 'ja'"
+        src="~/assets/images/layouts/site-logo.png"
+        class="ferris-logo"
+        width="200"
+        height="65"
+        alt="フェリス女学院大学附属図書館"
+      />
+    </templates-a-header>
+    <templates-a-main>
+      <slot />
+    </templates-a-main>
+    <templates-a-footer :copyright="copyright"></templates-a-footer>
+    <templates-to-top></templates-to-top>
+    <templates-confirm-download></templates-confirm-download>
   </v-app>
 </template>
---
+++ components/templates/AMain.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useLoading } from "@/composable/layout/useLoading";
+const { loading } = useLoading();
+</script>
+
+<template>
+  <v-main>
+    <v-skeleton-loader
+      :loading="loading"
+      max-width="800"
+      type="image, subtitle, sentences"
+    >
+      <slot />
+    </v-skeleton-loader>
+  </v-main>
+</template>
