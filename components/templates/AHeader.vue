e38cb5535216ae2b466f17c06d428f4b4e6f4cbd
saginoya <saginoya@create-nippo.co.jp>
2024-02-29 11:45:01 +0900

    Update AHeader component

--- components/templates/AHeader.vue
+++ components/templates/AHeader.vue
@@ -5,28 +5,12 @@ const { inversion } = useNavigationDrawer();
 </script>
 
 <template>
-  <v-app-bar color="white">
-    <v-app-bar-nav-icon @click.stop="inversion"></v-app-bar-nav-icon>
-    <v-toolbar-title class="pl-2">
+  <v-app-bar>
+    <template #prepend>
+      <v-app-bar-nav-icon @click.stop="inversion"></v-app-bar-nav-icon>
+    </template>
+    <v-app-bar-title>
       <slot />
-    </v-toolbar-title>
+    </v-app-bar-title>
   </v-app-bar>
 </template>
-
-<style scoped>
-.ferris-logo {
-  vertical-align: middle;
-  max-width: 100%;
-}
-.site-title {
-  font-feature-settings: "palt";
-  font-weight: bold;
-}
-.fade-image-enter-active {
-  transition: all 0.5s ease-out;
-}
-.fade-image-enter {
-  transform: translateX(200px);
-  opacity: 0;
-}
-</style>
