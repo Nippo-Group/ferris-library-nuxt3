44cf61642b3b34be831b6db886881e9f42f4cf1b
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-03-31 13:18:23 +0900

    Update Consortium page

--- contents/consortium.ts
+++ contents/consortium.ts
@@ -1,5 +1,4 @@
-<script setup lang="ts">
-const items = [
+export const items = [
   {
     name: '神奈川大学図書館',
     url: 'https://www.kanagawa-u.ac.jp/library/',
@@ -52,24 +51,13 @@ const items = [
     url: 'https://opac.yokohama-cu.ac.jp/drupal/',
   },
 ]
-</script>
 
-<template>
-  <VCard>
-    <VCardTitle> 大学名一覧（50音順） </VCardTitle>
-    <VList>
-      <VListItem
-        v-for="(item, index) in items"
-        :key="index"
-        :link="Boolean(item.url)"
-        :href="item.url"
-        target="_blank"
-        :title="item.name"
-      >
-        <template #append>
-          <icons-open-in-new v-if="item.url" />
-        </template>
-      </VListItem>
-    </VList>
-  </VCard>
-</template>
+export const note = `大学名一覧の13大学の学生・教職員は、以下のとおりそれぞれの図書館を利用することができます。
+
+**１．所蔵情報の提供**
+各大学図書館ホームページから、オンライン蔵書検索OPAC（Online Public Access Catalog）によって、所蔵情報を検索することができます。
+
+**２．図書館の館内利用**
+原則として、学生証又は大学発行の身分証明書を提示し、手続きすることで、館内利用ができます。
+
+（手続き方法は、各館によって異なります。事前連絡、紹介状が必要な館もあります。また、利用時期等に制限がある場合もありますので、記載事項を確認してください。東洋英和女学院大学図書館、フェリス女学院大学附属図書館の場合学生による利用は女子に限ります。）`
--- pages/consortium.vue
+++ pages/consortium.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { items, note } from '@/contents/consortium'
+
 const title = '横浜市内大学図書館コンソーシアム'
 const description
   = '横浜市内大学図書館コンソーシアムは、「横浜市内大学間学術・教育交流協議会」の会員校によって、学生の教育機会の多様化を図ること、相互に各図書館の特性を生かしつつ協力し、交流を深め、教育・研究活動の推進を図ることを目的として結成されました。'
@@ -23,12 +25,35 @@ useSeoMeta({
         <p>
           最新の情報は各館のホームページなどでご確認ください。
         </p>
-        <contents-consortium-list />
+      </VCol>
+      <VCol cols="12">
+        <VCard>
+          <VCardTitle> 大学名一覧（50音順） </VCardTitle>
+          <VList>
+            <VListItem
+              v-for="(item, index) in items"
+              :key="index"
+              :link="Boolean(item.url)"
+              :href="item.url"
+              target="_blank"
+              :title="item.name"
+            >
+              <template #append>
+                <icons-open-in-new v-if="item.url" />
+              </template>
+            </VListItem>
+          </VList>
+        </VCard>
       </VCol>
     </VRow>
     <VRow>
       <VCol cols="12">
-        <contents-consortium-notes />
+        <VAlert
+          density="compact"
+          type="success"
+        >
+          <PartsHtmlTextArea :markdown="note" />
+        </VAlert>
       </VCol>
     </VRow>
   </VContainer>
