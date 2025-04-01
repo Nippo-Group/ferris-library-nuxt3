11adf5f52a3148b286ef41b7720e64669aa2e1ca
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-04-01 13:52:46 +0900

    Update Off Campus page

--- pages/off-campus.vue
+++ pages/off-campus.vue
@@ -18,8 +18,20 @@ useSeoMeta({
     </VRow>
     <VRow>
       <VCol cols="12">
-        <contents-home-delivery />
+        <VCard>
+          <VCardTitle class="wrap-text">
+            新型コロナウイルス感染拡大防止措置：宅配による貸出
+          </VCardTitle>
+          <VCardText>2023年度前期授業方針に従い、2023年5月7日をもって終了しました。</VCardText>
+        </VCard>
       </VCol>
     </VRow>
   </VContainer>
 </template>
+
+<style scoped>
+.wrap-text {
+  word-break: break-all;
+  white-space: normal;
+}
+</style>
