f07a099c23ae6f212678a0b76ae824b6c5453c21
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-04-01 14:32:12 +0900

    Update Reading Project page

---
+++ contents/reading-project.ts
@@ -0,0 +1,99 @@
+import imgPlanning01 from '~/assets/images/reading-project/about-planning-01.png'
+import imgPlanning02 from '~/assets/images/reading-project/about-planning-02.png'
+import imgRecitation01 from '~/assets/images/reading-project/about-recitation-01.png'
+import imgRecitation02 from '~/assets/images/reading-project/about-recitation-02.png'
+
+export const isWhat
+  = '本学の読書運動プロジェクトは、1998年にシアトル市で始まり、シカゴ市で2001年以降大きな成果を収め全米各地に広まった《One Book, One Community.》にヒントを得たものです。この取組みは、選定された図書の読書を通じてコミュニティの意識を育むOneBook, One Chicagoとしてシカゴ公共図書館で現在も続いています。本学では2002年度から始まり、図書館を拠点として「読書」を個人的な経験だけでなく他者と共有・発信する活動を支援しています。'
+
+export const mainActivity = [
+  '本の展示',
+  'POPコンテスト・創作コンクール',
+  '選書ツアー',
+  '朗読会',
+  '各種イベントの実施',
+]
+export const documents = [
+  {
+    name: '活動報告書',
+    url: '/documents/reading-project/activity-report2020-2021.pdf',
+    type: 'PDF',
+  },
+  {
+    name: 'リーフレット',
+    url: '/documents/reading-project/activity-introduction-leaflet.pdf',
+    type: 'PDF',
+  },
+]
+
+export const recommendedFor = [
+  '本が好き',
+  'イラストを描くのが好き',
+  'イベントが好き、企画してみたい',
+  '大学生だからこそできる活動がしたい',
+  '人前で話すのは苦手だが、克服したい',
+]
+
+export const mediaInformation = {
+  title: '掲載されました！',
+  actions: [
+    {
+      url: 'https://magazine.ferris.ac.jp/20230511/18045/',
+      title: '読書運動プロジェクト活動報告　～2023春～',
+      source: 'フェリスを綴る　2023年5月11日掲載',
+    },
+    {
+      url: 'https://magazine.ferris.ac.jp/20230328/17905/',
+      title:
+          '読書運動プロジェクト：朗読会「しあわせの、かたち。 ―ひとりひとりが願うこと―」を開催しました',
+      source: 'フェリスを綴る　2023年3月28日掲載',
+    },
+    {
+      url: 'https://magazine.ferris.ac.jp/20221021/17101/',
+      title:
+          '読書体験の魅力を共有し、新たな本に出会う機会を創出する読書運動プロジェクト',
+      source: 'フェリスを綴る　2022年10月11日掲載',
+    },
+    {
+      url: 'https://magazine.ferris.ac.jp/20220830/16312/',
+      title: '読書運動プロジェクト活動報告　～2022夏～',
+      source: 'フェリスを綴る　2022年8月30日掲載',
+    },
+  ],
+}
+
+export const newsIds = [
+  'bp922vzsag',
+  '3-h0zb9femhn',
+  '2nqt2bwmsqd',
+  'zt10lnwef',
+  'tzndu8jahabw',
+  'rne-f461pg',
+]
+
+export const teamPlanning = {
+  name: '企画チーム',
+  content: '毎年テーマを決めて読書会や本の展示を行うほか、大学祭では展示発表やワークショップを主催します。<br />本をツールとしたおもしろい企画やアイデアいっぱいの広報を展開します。',
+  images: [imgPlanning01, imgPlanning02],
+}
+
+export const teamRecitation = {
+  name: '朗読チーム',
+  content: '朗読の専門家から直々に指導を受けられるので、 マイクなしで相手に伝わりやすい声、 表現方法が身につきます。<br />大学祭や文学館など、学内外で発表の機会があります。',
+  images: [imgRecitation01, imgRecitation02],
+  movie: {
+    src: 'https://www.youtube.com/embed/WcCruNTE__M',
+    title: '朗読動画（演目「やまなし」）',
+  },
+}
+
+export const movies = [
+  {
+    src: 'https://www.youtube.com/embed/HRKpRB5fZ-o',
+    title: '図書館施設・設備編',
+  },
+  {
+    src: 'https://www.youtube.com/embed/rBAip8vAxKU',
+    title: 'ラーニングコモンズ編',
+  },
+]
--- pages/reading-project.vue
+++ pages/reading-project.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
-import titleImage from '~/assets/images/reading-project/reading-project-title.png'
+import { useNews } from '@/composables/news/useNews'
+import titleImage from '@/assets/images/reading-project/reading-project-title.png'
+import {
+  isWhat,
+  mainActivity,
+  documents,
+  recommendedFor,
+  mediaInformation,
+  teamPlanning,
+  teamRecitation,
+  movies,
+  newsIds,
+} from '@/contents/reading-project'
 
 const title = '読書運動プロジェクト'
 const description
@@ -9,8 +21,11 @@ useSeoMeta({
   description,
 })
 
-const isWhat
-  = '本学の読書運動プロジェクトは、1998年にシアトル市で始まり、シカゴ市で2001年以降大きな成果を収め全米各地に広まった《One Book, One Community.》にヒントを得たものです。この取組みは、選定された図書の読書を通じてコミュニティの意識を育むOneBook, One Chicagoとしてシカゴ公共図書館で現在も続いています。本学では2002年度から始まり、図書館を拠点として「読書」を個人的な経験だけでなく他者と共有・発信する活動を支援しています。'
+const { show } = useConfirmDL()
+
+const { newsList } = useNews({
+  ids: newsIds.join(','),
+})
 </script>
 
 <template>
@@ -32,22 +47,110 @@ const isWhat
           />
         </VCol>
         <VCol cols="12">
-          <contents-reading-project-main-activity />
+          <templates-card-reading-project title="主な活動">
+            <VContainer>
+              <VRow dense>
+                <VCol
+                  cols="12"
+                  md="6"
+                >
+                  <PartsNumberedList :items="mainActivity" />
+                </VCol>
+                <VCol
+                  v-for="(document, index) in documents"
+                  :key="`document${index}`"
+                  cols="12"
+                  sm="6"
+                  md="3"
+                >
+                  <client-only>
+                    <templates-pdf-viewer
+                      :src="document.url"
+                      button-hidden
+                      pagination-hidden
+                    />
+                  </client-only>
+                  <VBtn
+                    block
+                    variant="text"
+                    @click="show(document.name, document.url, document.type)"
+                  >
+                    {{ document.name }}
+                    <icons-file-pdf end />
+                  </VBtn>
+                </VCol>
+              </VRow>
+            </VContainer>
+          </templates-card-reading-project>
         </VCol>
         <VCol
           cols="12"
           lg="6"
         >
-          <contents-reading-project-recruting />
+          <templates-card-reading-project title="《学内のみなさまへ》学生メンバー募集中！">
+            <VCardText>
+              図書館や本の魅力を学内外の様々な人に伝える活動を、一緒にしてみませんか？<br>学年不問で、いつからでも参加できます。
+            </VCardText>
+            <VList>
+              <VListSubheader>【こんな人におすすめ！】</VListSubheader>
+              <VListItem
+                v-for="(item, index) in recommendedFor"
+                :key="index"
+                rounded="xl"
+                density="compact"
+              >
+                <VListItemTitle class="wrap-text">
+                  {{ item }}
+                </VListItemTitle>
+                <template #prepend>
+                  <icons-check-marked />
+                </template>
+              </VListItem>
+            </VList>
+            <VCardText>
+              参加希望の方、もっと詳しく活動について知りたい方は、図書館カウンターもしくは下記メールアドレスまでお問い合わせください。
+            </VCardText>
+            <TemplatesBannerEmail
+              address="r_library@ferris.ac.jp"
+            />
+          </templates-card-reading-project>
         </VCol>
         <VCol
           cols="12"
           lg="6"
         >
-          <contents-reading-project-notice />
+          <templates-card-reading-project :title="mediaInformation.title">
+            <VList
+              v-if="mediaInformation.actions"
+              lines="two"
+            >
+              <template
+                v-for="(media, index) in mediaInformation.actions"
+                :key="index"
+              >
+                <VDivider v-show="index > 0" />
+                <VListItem
+                  :href="media.url"
+                  target="_blank"
+                >
+                  <VListItemTitle class="wrap-text">
+                    {{
+                      media.title
+                    }}
+                  </VListItemTitle>
+                  <VListItemSubtitle>{{ media.source }}</VListItemSubtitle>
+                  <template #append>
+                    <icons-open-in-new />
+                  </template>
+                </VListItem>
+              </template>
+            </VList>
+          </templates-card-reading-project>
         </VCol>
         <VCol cols="12">
-          <contents-reading-project-news />
+          <templates-card-reading-project title="News">
+            <templates-list-news :contents-list="newsList" />
+          </templates-card-reading-project>
         </VCol>
       </VRow>
       <VRow>
@@ -61,13 +164,51 @@ const isWhat
           cols="12"
           md="6"
         >
-          <contents-reading-project-team-planning />
+          <templates-card-reading-project :title="teamPlanning.name">
+            <VCardText>
+              <PartsHtmlTextArea :data="teamPlanning.content" />
+            </VCardText>
+            <VContainer>
+              <VRow dense>
+                <VCol
+                  v-for="(image, imgIndex) in teamPlanning.images"
+                  :key="imgIndex"
+                  cols="12"
+                  sm="6"
+                >
+                  <VImg :src="image" />
+                </VCol>
+              </VRow>
+            </VContainer>
+          </templates-card-reading-project>
         </VCol>
         <VCol
           cols="12"
           md="6"
         >
-          <contents-reading-project-team-recitation />
+          <templates-card-reading-project :title="teamRecitation.name">
+            <VCardText>
+              <PartsHtmlTextArea :data="teamRecitation.content" />
+            </VCardText>
+            <VContainer>
+              <VRow dense>
+                <VCol
+                  v-for="(image, imgIndex) in teamRecitation.images"
+                  :key="imgIndex"
+                  cols="12"
+                  sm="6"
+                >
+                  <VImg :src="image" />
+                </VCol>
+              </VRow>
+            </VContainer>
+            <VCardText v-if="teamRecitation.movie">
+              <TemplatesYoutubeViewer
+                :title="teamRecitation.movie.title"
+                :src="teamRecitation.movie.src"
+              />
+            </VCardText>
+          </templates-card-reading-project>
         </VCol>
       </VRow>
       <VRow>
@@ -77,7 +218,23 @@ const isWhat
           />
         </VCol>
         <VCol cols="12">
-          <contents-reading-project-activity-base />
+          <templates-card-reading-project title="図書館紹介動画">
+            <VContainer>
+              <VRow dense>
+                <VCol
+                  v-for="(item, index) in movies"
+                  :key="`video-${index}`"
+                  cols="12"
+                  md="6"
+                >
+                  <TemplatesYoutubeViewer
+                    :title="item.title"
+                    :src="item.src"
+                  />
+                </VCol>
+              </VRow>
+            </VContainer>
+          </templates-card-reading-project>
         </VCol>
       </VRow>
       <VRow>
@@ -85,7 +242,16 @@ const isWhat
           <SectionsRPHeaderLv2 title="お問い合わせ" />
         </VCol>
         <VCol cols="12">
-          <contents-reading-project-contact-address />
+          <templates-card-reading-project title="お問い合わせ">
+            <VCardText>
+              <p>
+                読書運動プロジェクトについてのお問い合わせは下記メールアドレスまでお願いいたします。
+              </p>
+            </VCardText>
+            <TemplatesBannerEmail
+              address="r_library@ferris.ac.jp"
+            />
+          </templates-card-reading-project>
         </VCol>
       </VRow>
     </VContainer>
@@ -96,5 +262,10 @@ const isWhat
 .page-title {
   white-space: nowrap;
   line-height: 1.2em;
+}
+
+.wrap-text {
+  word-break: break-all;
+  white-space: normal;
 }
 </style>
