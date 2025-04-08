f37a1544512ec09b03b1985947203bc9a5bbf194
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-04-08 14:50:08 +0900

    Update Layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { useLanguage } from '@/composables/common/'
+import { globalNavigation } from '@/contents/globalNavigation'
 
 useLanguage().setLang('ja')
 
-const siteTitle = ref('フェリス女学院大学附属図書館')
+const siteTitle = 'フェリス女学院大学附属図書館'
+const copyright = 'Ferris University Library'
+
 useHead({
   titleTemplate: (title) => {
     return title ? `${title} - ${siteTitle.value}` : siteTitle.value
@@ -15,7 +18,22 @@ useHead({
 </script>
 
 <template>
-  <SectionsLayoutWrapper>
-    <slot />
-  </SectionsLayoutWrapper>
+  <VApp id="inspire">
+    <SectionsNavigationDrawer :menus="globalNavigation" />
+    <SectionsHeader>
+      <img
+        src="~/assets/images/layouts/site-logo.png"
+        class="ferris-logo"
+        width="200"
+        height="65"
+        :alt="siteTitle"
+      >
+    </SectionsHeader>
+    <SectionsMain>
+      <slot />
+    </SectionsMain>
+    <SectionsFooter :copyright="copyright" />
+    <SectionsToTop />
+    <TemplatesConfirmDownload />
+  </VApp>
 </template>
--- layouts/english.vue
+++ layouts/english.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { useLanguage } from '@/composables/common/'
+import { globalNavigationEng } from '@/contents/globalNavigation'
 
 useLanguage().setLang('en')
 
-const siteTitle = ref('Ferris University Library')
+const siteTitle = 'Ferris University Library'
+
 useHead({
   titleTemplate: (title) => {
     return title ? `${title} - ${siteTitle.value}` : siteTitle.value
@@ -15,7 +17,22 @@ useHead({
 </script>
 
 <template>
-  <SectionsLayoutWrapper>
-    <slot />
-  </SectionsLayoutWrapper>
+  <VApp id="inspire">
+    <SectionsNavigationDrawer :menus="globalNavigationEng" />
+    <SectionsHeader>
+      <img
+        src="~/assets/images/layouts/site-logo-english.png"
+        class="ferris-logo"
+        width="120"
+        height="34"
+        :alt="siteTitle"
+      >
+    </SectionsHeader>
+    <SectionsMain>
+      <slot />
+    </SectionsMain>
+    <SectionsFooter :copyright="siteTitle" />
+    <SectionsToTop />
+    <TemplatesConfirmDownload />
+  </VApp>
 </template>
