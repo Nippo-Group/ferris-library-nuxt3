642a52187d77eb759198124d1bb1c3f1f2171db5
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-07-18 15:24:11 +0900

    Update HtmlTextArea

--- components/parts/HtmlTextArea.vue
+++ components/parts/HtmlTextArea.vue
@@ -9,14 +9,15 @@ defineProps<{
 
 <template>
   <article
-    class="prose text"
+    class="text"
     v-html="data || converterMarkdown(markdown || '')"
   />
 </template>
 
 <style scoped>
-.text::v-deep(h1) {
-  margin-bottom: 3rem;
+.text::v-deep(*) {
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
 }
 .text::v-deep(h2) {
   margin-top: 3rem;
@@ -33,17 +34,33 @@ defineProps<{
 .text::v-deep(h6) {
   margin-top: 1rem;
 }
-.text::v-deep(p) {
-  margin-bottom: 1rem;
-}
-
-.text::v-deep(ul),
-.text::v-deep(ol) {
-  padding-left: 2em;
-  margin-bottom: 1rem;
-}
 .text::v-deep(img) {
   max-width: 100%;
   height: auto;
+}
+.text::v-deep(ul),
+.text::v-deep(ol) {
+  padding-left: 1.5em;
+}
+.text::v-deep(dt) {
+  font-weight: bold;
+}
+.text::v-deep(dd) {
+  padding-left: 1em;
+}
+.text::v-deep(table) {
+  display: block;
+  border-spacing: 0;
+  overflow-x: scroll;
+  white-space: nowrap;
+  -webkit-overflow-scrolling: touch;
+}
+.text::v-deep(th),
+.text::v-deep(td) {
+  border: solid 1px #ddd;
+  padding: 10px 5px;
+}
+.text::v-deep(th) {
+  background-color: #eee;
 }
 </style>
