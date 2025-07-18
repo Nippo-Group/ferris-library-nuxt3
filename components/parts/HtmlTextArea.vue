f31a6dce5c6899a0ac5447d89e43a7ddef6bd3a5
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-07-18 14:12:11 +0900

    Update HtmlTextArea

--- components/parts/HtmlTextArea.vue
+++ components/parts/HtmlTextArea.vue
@@ -8,69 +8,42 @@ defineProps<{
 </script>
 
 <template>
-  <div
-    class="text"
+  <article
+    class="prose text"
     v-html="data || converterMarkdown(markdown || '')"
   />
 </template>
 
 <style scoped>
-.text {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-}
 .text::v-deep(h1) {
-  font-size: 1.8rem;
-  font-weight: bold;
+  margin-bottom: 3rem;
 }
 .text::v-deep(h2) {
-  font-size: 1.5rem;
-  font-weight: bold;
+  margin-top: 3rem;
 }
 .text::v-deep(h3) {
-  font-size: 1.25rem;
-  font-weight: bold;
+  margin-top: 2rem;
 }
 .text::v-deep(h4) {
-  font-size: 1.125rem;
+  margin-top: 1rem;
 }
 .text::v-deep(h5) {
-  font-size: 1rem;
+  margin-top: 1rem;
 }
 .text::v-deep(h6) {
-  font-size: 0.875rem;
+  margin-top: 1rem;
 }
+.text::v-deep(p) {
+  margin-bottom: 1rem;
+}
+
 .text::v-deep(ul),
 .text::v-deep(ol) {
-  padding-left: 1.5em;
-}
-.text::v-deep(dt) {
-  font-weight: bold;
-}
-.text::v-deep(dd) {
-  padding-left: 1em;
-}
-.text::v-deep(table) {
-  display: block;
-  border-spacing: 0;
-  overflow-x: scroll;
-  white-space: nowrap;
-  -webkit-overflow-scrolling: touch;
-}
-.text::v-deep(th),
-.text::v-deep(td) {
-  border: solid 1px #ddd;
-  padding: 10px 5px;
-}
-.text::v-deep(th) {
-  background-color: #eee;
+  padding-left: 2em;
+  margin-bottom: 1rem;
 }
 .text::v-deep(img) {
   max-width: 100%;
   height: auto;
-}
-.text::v-deep(caption) {
-  text-align: left;
 }
 </style>
