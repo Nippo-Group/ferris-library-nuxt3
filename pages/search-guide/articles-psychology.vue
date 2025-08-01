e32ffa11007859363c586142faad93e474b890c8
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-08-01 17:04:52 +0900

    Update Search Guide contents & Pages

---
+++ pages/search-guide/articles-psychology.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { contentsMap } from '~/contents/search-guide'
+
+const title = '文献探索ガイド・リンク集'
+useSeoMeta({
+  title,
+})
+
+const id = 'articles-psychology'
+</script>
+
+<template>
+  <VContainer>
+    <VRow>
+      <VCol cols="12">
+        <TemplatesToolbarSearchGuide />
+      </VCol>
+      <VCol cols="12">
+        <VCard>
+          <VCardText
+            class="text-area"
+          >
+            <PartsHtmlTextArea
+              :markdown="
+                contentsMap[id]"
+            />
+          </VCardText>
+        </VCard>
+      </VCol>
+    </VRow>
+  </VContainer>
+</template>
---
+++ pages/search-guide/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { items, type Contents } from '~/contents/search-guide'
+
+const title = '文献探索ガイド・リンク集'
+useSeoMeta({
+  title,
+})
+
+const router = useRouter()
+const action = (id: Contents) => {
+  router.push(`/search-guide/${id}`)
+}
+</script>
+
+<template>
+  <VContainer>
+    <VRow>
+      <VCol>
+        <SectionsPageHeader
+          :title
+        />
+      </VCol>
+    </VRow>
+    <VRow>
+      <VCol>
+        <VCard>
+          <TemplatesListSearchGuide
+            :items
+            :action="action"
+          />
+        </VCard>
+      </VCol>
+    </VRow>
+  </VContainer>
+</template>
