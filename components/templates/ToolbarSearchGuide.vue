d2e786f6ecbce0f1fae1f4a42302882682a44649
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-08-01 17:05:37 +0900

    Add ToolbarSearchGuide

---
+++ components/templates/ToolbarSearchGuide.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { items, type Contents } from '~/contents/search-guide'
+import { iconMap } from '~/utils'
+
+const title = '文献探索ガイド・リンク集'
+useSeoMeta({
+  title,
+})
+
+const router = useRouter()
+const back = () => {
+  router.push(`/search-guide`)
+}
+
+const action = (id: Contents) => {
+  router.push(`/search-guide/${id}`)
+}
+</script>
+
+<template>
+  <VToolbar>
+    <VToolbarTitle :text="title" />
+    <template #append>
+      <VFadeTransition>
+        <VBtn
+          :prepend-icon="iconMap['chevronLeft']"
+          @click="back()"
+        >
+          目次
+        </VBtn>
+      </VFadeTransition>
+      <VMenu location="bottom">
+        <template #activator="{ props }">
+          <VBtn
+            v-bind="props"
+            :icon="iconMap['dots']"
+          />
+        </template>
+        <VCard max-height="80vh">
+          <TemplatesListSearchGuide
+            :items
+            :action="action"
+          />
+        </VCard>
+      </VMenu>
+    </template>
+  </VToolbar>
+</template>
