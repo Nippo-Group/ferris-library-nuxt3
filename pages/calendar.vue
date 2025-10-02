433ae72b3a0a61965691a34e87b1c844f5769f98
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-10-02 12:58:04 +0900

    Update Vuetify

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -17,7 +17,7 @@ useSeoMeta({
     </VRow>
     <VRow>
       <VCol cols="12">
-        <SectionsOpeningCalendar />
+        <SectionsOpeningCalendarV />
       </VCol>
     </VRow>
   </VContainer>
