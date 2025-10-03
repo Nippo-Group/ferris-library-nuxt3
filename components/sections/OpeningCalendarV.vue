34f0245d5503969e9eba8927f76d7899c5676ea8
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-10-03 16:11:52 +0900

    Update OpeningCalendar Beta

--- components/sections/OpeningCalendarV.vue
+++ components/sections/OpeningCalendarV.vue
@@ -2,66 +2,251 @@
 import { VCalendar } from 'vuetify/labs/VCalendar'
 
 import { ref } from 'vue'
+import jsonRyokuen from '@/assets/json/calendar-ryokuen.json'
+import jsonYamate from '@/assets/json/calendar-yamate.json'
+import jsonCommon from '@/assets/json/calendar-common.json'
 
-const type = ref('month')
+import { iconMap } from '@/utils'
+import { useLanguage } from '@/composables/common/'
 
-const mode = ref('stack')
+/*
+モジュールの読み込み
+------------ */
 
-const weekday = ref([0, 1, 2, 3, 4, 5, 6])
+// 言語の状態
+const { langState } = useLanguage()
 
-const value = ref('')
-const events = ref([])
-const colors = ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey-darken-1']
-const names = ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party']
+// それぞれのイベントを共通イベントと結合
+const eventsRyokuen = jsonRyokuen.concat(jsonCommon)
+const eventsYamate = jsonYamate.concat(jsonCommon)
 
-const rnd = (a: number, b: number): number => {
-  return Math.floor((b - a + 1) * Math.random()) + a
+/*
+定数
+------------ */
+
+// 図書館の種類ごとの名称
+const locationLabel = {
+  ryokuen: {
+    en: 'Ryokuen',
+    ja: '緑園本館',
+  },
+  yamate: {
+    en: 'Yamate',
+    ja: '山手分室',
+  },
+} as const
+
+// 時間帯指定の正規表現
+const regexTimeSpecification = /^\d{2}:\d{2}\s*.+\s*\d{2}:\d{2}$/
+
+// イベントと色の対応表
+const eventColorsMap = {
+  'default': '#5C6BC0',
+  'closed': '#BDBDBD',
+  '8:50-21:00': '#42A5F5',
+  '9:00-19:00': '#FFA726',
+  '9:00-17:00': '#66BB6A',
+  '9:00-18:00': '#FFCA28',
+  '8:50-18:30': '#AB47BC',
+  '10:00-15:00': '#FF7043',
+  '8:50-18:00': '#D81B60',
+  '10:00-17:00': '#C0CA33',
+} as const
+/*
+型定義
+------------ */
+
+// イベントの形式
+type Event = {
+  name: string
+  start: string
+  end?: string
+  color?: string
+  timed?: boolean
 }
 
-const getEvents = ({ start, end }) => {
-  const evts = []
+// 図書館の種類
+type Location = 'ryokuen' | 'yamate'
 
-  const min = new Date(`${start.date}T00:00:00`)
-  const max = new Date(`${end.date}T23:59:59`)
-  const days = (max.getTime() - min.getTime()) / 86400000
-  const eventCount = rnd(days, days + 20)
+// カレンダーコンポーネントのタイプ
+type CalendarType = 'month' | 'category' | 'day' | '4day' | 'custom-daily' | 'custom-weekly' | 'week' | undefined
 
-  for (let i = 0; i < eventCount; i++) {
-    const allDay = rnd(0, 3) === 0
-    const firstTimestamp = rnd(min.getTime(), max.getTime())
-    const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-    const secondTimestamp = rnd(2, allDay ? 288 : 8) * 900000
-    const second = new Date(first.getTime() + secondTimestamp)
+type EventType = keyof typeof eventColorsMap
 
-    evts.push({
-      name: names[rnd(0, names.length - 1)],
-      start: first,
-      end: second,
-      color: colors[rnd(0, colors.length - 1)],
-      timed: !allDay,
-    })
+/*
+状態管理
+------------ */
+
+// カレンダーコンポーネント
+const calendar = ref()
+
+// 図書館の種類
+const location = ref<Location>('ryokuen')
+
+// フォーカスされてる日付
+const focus = ref('')
+
+// カレンダーの種類
+const type = ref<CalendarType>('month')
+
+/*
+メソッド
+------------ */
+
+// 図書館の種類をセットする
+const setLocation = (value: Location): void => {
+  location.value = value
+}
+
+// フォーカスを今日にもどす
+const setToday = (): void => {
+  focus.value = ''
+}
+
+// 先月・前日へ
+const prev = (): void => {
+  calendar.value.prev()
+}
+
+// 翌月・翌日へ
+const next = (): void => {
+  calendar.value.next()
+}
+
+/*
+ユーティリティー
+------------ */
+
+// 時間帯の指定方法をフォーマットする
+const formatTimeRange = (input: string): string | undefined => {
+  // 時刻(hh:mm)をすべて抽出
+  const matches = input.match(/\d{1,2}:\d{2}/g)
+
+  if (!matches || matches.length < 2) {
+    return undefined // 時刻が2つ未満ならフォーマット不可
   }
 
-  events.value = evts
+  // 先頭と次の時刻を取得
+  let [start, end] = matches
+
+  // 桁揃え（1桁の時間は2桁に直す）
+  start = start.replace(/^(\d{1}):/, '0$1:')
+  end = end.replace(/^(\d{1}):/, '0$1:')
+
+  // コロン後の分も2桁に（例: 09:5 → 09:05）
+  start = start.replace(/:(\d{1})$/, ':0$1')
+  end = end.replace(/:(\d{1})$/, ':0$1')
+
+  return `${start}-${end}`
 }
 
-function getEventColor(event) {
-  return event.color
+// イベントカラーを取得する
+const getEventColor = (name: string): string => {
+  if (name === 'Closed' || name === '閉館' || name === '閉室') {
+    return eventColorsMap['closed']
+  }
+  else if (regexTimeSpecification.test(name)) {
+    return eventColorsMap[formatTimeRange(name) as EventType]
+  }
+  else {
+    return eventColorsMap['default']
+  }
 }
+
+// イベントをフォーマットする
+const eventFormat = (events: Event[]): Event[] => {
+  const arr = []
+  for (const event of events) {
+    arr.push({
+      name: renameClosed(event.name),
+      color: event.color ?? getEventColor(event.name),
+      start: event.start,
+      end: event.end ? `${event.end} 24:00:00` : undefined,
+      timed: true,
+    })
+  }
+  return arr
+}
+
+// 英語ページの場合は閉館・閉室を変換する関数
+const renameClosed = (eventName: string): string => {
+  if (langState.value === 'ja') return eventName
+
+  if (eventName === '閉館' || eventName === '閉室') {
+    return 'Closed'
+  }
+  else {
+    return eventName
+  }
+}
+
+/*
+演算
+------------ */
+
+// 最終的なイベントリスト
+const events = computed<Event[]>(() => {
+  if (location.value === 'ryokuen') {
+    return eventFormat(eventsRyokuen)
+  }
+  else {
+    return eventFormat(eventsYamate)
+  }
+})
 </script>
 
 <template>
-  <v-sheet height="600">
-    <v-calendar
-      ref="calendar"
-      v-model="value"
-      :event-color="getEventColor"
-      :event-overlap-mode="mode"
-      :event-overlap-threshold="30"
-      :events="events"
-      :type="type"
-      :weekdays="weekday"
-      @change="getEvents"
-    />
-  </v-sheet>
+  <div>
+    <VToolbar>
+      <VMenu>
+        <template #activator="{ props }">
+          <VBtn
+            v-bind="props"
+            color="primary"
+            variant="outlined"
+            :prepend-icon="iconMap['menuDown']"
+          >
+            {{ locationLabel[location][langState] }}
+          </VBtn>
+        </template>
+        <VList>
+          <VListItem
+            :title="locationLabel.ryokuen[langState]"
+            @click="setLocation('ryokuen')"
+          />
+          <VListItem
+            :title="locationLabel.yamate[langState]"
+            @click="setLocation('yamate')"
+          />
+        </VList>
+      </VMenu>
+      <VToolbarTitle v-if="calendar">
+        {{ calendar.title }}
+      </VToolbarTitle>
+
+      <VBtn
+        :icon="iconMap['chevronLeft']"
+        @click="prev"
+      />
+      <VBtn
+        :icon="iconMap['chevronRight']"
+        @click="next"
+      />
+      <VBtn
+        :icon="iconMap['calendarToday']"
+        @click="setToday"
+      />
+    </VToolbar>
+    <VSheet
+      class="mt-4"
+      height="600"
+    >
+      <VCalendar
+        ref="calendar"
+        v-model="focus"
+        :type
+        :events
+      />
+    </VSheet>
+  </div>
 </template>
