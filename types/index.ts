93de0364ce51b575359b773917b007d2fc7a4f96
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-10-06 15:22:58 +0900

    Update Types

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,3 @@
-export * from './common'
+export * from './link'
+export * from './lang'
+export * from './location'
--- types/lang.ts
+++ types/lang.ts
@@ -1,1 +1,3 @@
 export type Lang = 'ja' | 'en'
+
+export type LangRecord<T> = Record<Lang, T>
