cda377a8297f84fa3f244de19447d59c14d8b720
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-11-13 14:46:19 +0900

    Update Eslint Config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,10 @@
-import stylistic from '@stylistic/eslint-plugin' // importも追加
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   {
     files: ['**/*.vue', '**/*.ts'],
     rules: {
-      'no-console': 'error',
-      '@typescript-eslint/no-explicit-any': 'error',
+      'no-console': 'warn',
     },
   },
-  // ここから追加
-  stylistic.configs.customize({
-    indent: 2, // インデントはスペース2
-    quotes: 'single', // クオートはシングル
-    semi: false, // セミコロンは不要
-  }),
 )
