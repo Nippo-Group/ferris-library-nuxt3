e31d0b9e5ef214d6e5549f5793e7035befb4d93b
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2025-11-17 13:15:12 +0900

    Update component/section/Main

--- app/components/sections/Main.vue
+++ app/components/sections/Main.vue
@@ -1,18 +1,7 @@
-<script setup lang="ts">
-import { useLoading } from '@/composables/layout/useLoading'
-
-const { loading } = useLoading()
-</script>
-
 <template>
   <VMain>
     <VSheet max-width="960">
-      <VSkeletonLoader
-        :loading="loading"
-        type="image, subtitle, sentences"
-      >
-        <slot />
-      </VSkeletonLoader>
+      <slot />
     </VSheet>
   </VMain>
 </template>
