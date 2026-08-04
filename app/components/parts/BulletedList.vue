ef906f32df084afddd26831bf3987c29b299d870
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2026-08-04 12:59:40 +0900

    feat: BulletedListコンポーネントの追加

---
+++ app/components/parts/BulletedList.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+/**
+ * @file 箇条書きリストを表示するためのコンポーネント
+ * @description リストの内容はスロットまたはpropsで指定でき、スロットが優先される。
+ *
+ * @props {Array<string>} items - 箇条書きのリスト
+ * @props {'unordered' | 'ordered'} type - リストの種類
+ */
+
+withDefaults(defineProps<{
+  items?: string[]
+  type?: 'unordered' | 'ordered'
+}>(), {
+  type: 'unordered',
+})
+</script>
+
+<template>
+  <component
+    :is="type === 'unordered' ? 'ul' : 'ol'"
+    class="list"
+  >
+    <slot>
+      <li
+        v-for="(item, index) in items"
+        :key="index"
+      >
+        {{ item }}
+      </li>
+    </slot>
+  </component>
+</template>
+
+<style scoped>
+.list {
+  padding-left: 1.5rem;
+  margin: 0;
+}
+.list > :deep(li) {
+  padding: 0.5rem 0;
+}
+</style>
