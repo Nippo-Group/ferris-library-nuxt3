441731decdc2fe98df0e2de829efb493b50ef871
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2026-08-04 11:37:02 +0900

    feat: DescriptionListコンポーネントの追加

---
+++ app/components/parts/DescriptionList.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+/**
+ * @file 用語と説明が対になったリストを表示するためのコンポーネント
+ * @description 子要素としてPartsDescriptionListItemを使用する
+ *
+ * @props {Array<{term: string, description: string}>} items - 用語と説明のリスト
+ */
+defineProps<{
+  items: {
+    term: string
+    description: string
+  }[]
+}>()
+</script>
+
+<template>
+  <dl class="list">
+    <slot>
+      <PartsDescriptionListItem
+        v-for="(item, index) in items"
+        :key="index"
+        :term="item.term"
+        :description="item.description"
+      />
+    </slot>
+  </dl>
+</template>
+
+<style scoped>
+.list > * {
+  border-bottom: 1px solid #ccc;
+}
+
+.list > *:first-child {
+  border-top: 1px solid #ccc;
+}
+</style>
---
+++ app/components/parts/DescriptionListItem.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+/**
+ * @file 用語と説明が対になったリストの子要素を表示するためのコンポーネント
+ * @description 親要素はPartsDescriptionListを使用する
+ *
+ * @props {string} term - 用語
+ * @props {string} description - 説明
+ */
+
+defineProps<{
+  term: string
+  description: string
+}>()
+</script>
+
+<template>
+  <div class="item">
+    <dt class="term">
+      {{ term }}
+    </dt>
+    <dd class="description">
+      <slot>{{ description }}</slot>
+    </dd>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.term,
+.description {
+  box-sizing: border-box;
+  padding: 1rem;
+}
+
+.term {
+  flex: 0 0 33.333%;
+  max-width: 33.333%;
+  color: #b71c1c;
+  background-color: #f6e2e2;
+  word-break: break-word;
+}
+
+.description {
+  flex: 1 1 0;
+  margin: 0;
+  min-width: 0;
+  word-break: break-word;
+}
+
+@media (max-width: 768px) {
+  .item {
+    flex-direction: column;
+  }
+
+  .term,
+  .description {
+    flex: 1 0 100%;
+    max-width: 100%;
+  }
+}
+</style>
